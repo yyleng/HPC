f448f8b3337c89ac8f6aecb842d3b13600807176
Your Name <you@example.com>
2025-02-25 23:25:57 +0800

    temp

--- pytorch/src/1_DeepLearningWithPytorch/7_image_learning/a.py
+++ pytorch/src/1_DeepLearningWithPytorch/7_image_learning/a.py
@@ -10,3 +10,19 @@ pil_transform = transforms.Compose([
 cifar10 = datasets.CIFAR10(data_path, train=True, download=True,transform=pil_transform)
 cifar10_val = datasets.CIFAR10(data_path, train=False, download=True,transform=pil_transform)
 
+# 飞机（0）
+# 汽车（1）
+# 鸟（2）
+# 猫（3）
+# 鹿（4）
+# 狗（5）
+# 青蛙（6）
+# 马（7）
+# 船 （8）
+# 卡车（9）
+
+# only keep airplane and bird
+label_map = {0: 0, 2: 1}
+class_names = ['airplane', 'bird']
+cifar2 = [(img, label_map[label]) for img, label in cifar10 if label in [0, 2]]
+cifar2_val = [(img, label_map[label]) for img, label in cifar10_val if label in [0, 2]]
